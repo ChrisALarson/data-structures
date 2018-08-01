b4a4ae6864f01861c04f7510e878f0126e041fc5
Chris Larson <c.lars701@gmail.com>
2018-08-01 13:29:40 -0700

    fix es6 stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,36 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  pop() {
+    if (this.count > 0) {
+      let lastItem = this.storage[this.count]; 
+      delete this.storage[this.count];
+      this.count--;
+      return lastItem;
+    }
+    console.log('in pop method');
+  }
+
+  size() {
+    return this.count;
+  }
 }
+
+/*
+  pop() {
+    if (this.count > 0) {
+      let lastItem = this.storage[this.count]; // why doesn't this work in the class?
+      delete lastItem;
+      this.count--;
+      return lastItem;
+    }
+  }
+  */
