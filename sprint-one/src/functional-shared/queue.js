a4dba0b8a6aa5b6547d5f7f4ac6dafa5f69b1840
Chris Larson <c.lars701@gmail.com>
2018-08-01 12:40:19 -0700

    fix functional shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,42 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+let Queue = function() {
+  let queue = {};
+  queue.storage = {};
+  queue.firstIndex = 0;
+  queue.lastIndex = 0;
+  queue.count = 0;
+
+  let extend = function(target, source) {
+    for (let key in source) {
+      target[key] = source[key];
+    }
+  };
+
+  extend(queue, queueMethods);
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    if (this.firstIndex === 0) {
+      this.firstIndex++;
+    }
+    this.lastIndex++;
+    this.storage[this.lastIndex] = value;
+    this.count = this.lastIndex - this.firstIndex + 1;
+  },
+  dequeue: function() {
+    if (this.count > 0) {
+      let firstItem = this.storage[this.firstIndex];
+      delete firstItem;
+      this.firstIndex++;
+      this.count = this.lastIndex - this.firstIndex + 1;
+      return firstItem;
+    }
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
