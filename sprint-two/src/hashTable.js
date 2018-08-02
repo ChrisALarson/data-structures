a61773c36b5c0d1a5fd957d4a3bb049c7725c3e6
ChrisALarson <c.lars701@gmail.com>
2018-08-01 22:48:01 -0700

    Completed through set.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,21 +2,33 @@
 
 var HashTable = function() {
   this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  // this._storage = LimitedArray(this._limit);
+  this._storage = [];
 };
+
+// let getIndexBelowMaxForKey = function(key, limit) {
+//   let index = parseInt(key[0], 32);
+//   return Math.floor(index / 10);
+// };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = undefined;
 };
 
+// let ourHashTable = new HashTable();
+// console.log(ourHashTable);
+// console.log('GetIndexBelowMaxForKey: ' + getIndexBelowMaxForKey('val1', 8));
 
 
 /*
